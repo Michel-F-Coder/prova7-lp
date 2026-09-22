413e198cbfbce31ee7647263b981befd64bce3fd
Michel-F-Coder <bust22b@gmail.com>
2026-09-22 20:42:00 -0300

    Create boletim.c

---
+++ boletim.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+float nota1, nota2, nota3, nota4;
+char aluno[50];
+
+
+
+
+
+
+void main()
+{
+
+    printf("================ BOLETIM ====================\n\n");
+    printf("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
